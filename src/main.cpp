4f7948123861497dcbd0edefa07d1d02c7a1e056
team98-k <83960170+team98-k@users.noreply.github.com>
2022-02-04 14:24:19 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+using namespace std;
+
+template <class T1, class T2>
+void ShowData(double num)
+{
+    cout<<(T1)num<<", "<<(T2)num<<endl;
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    ShowData<char, double>(65);
     return 0;
 }
