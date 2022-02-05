e2c35b37f91d15eb01d2d3e3b38abc4180802b51
neogulcity <neogulcity@gmail.com>
2022-02-05 20:18:10 +0900

    Update hashtable_2.h
    Add string hashtable

--- src/hashtable_2.h
+++ src/hashtable_2.h
@@ -8,25 +8,18 @@ namespace hashtable
     class hashtable
     {
         uint16_t size;
+        uint16_t count;
         std::vector<std::string> data;
         std::vector<std::string> key;
-        int count;
 
     public:
         hashtable(uint16_t _size = 1000);
-        int hashing(std::string _key, bool isload = false);
-        int d_hashing(std::string _key, int h_key, bool isload);
+        int16_t d_hashing(std::string _key, int16_t h_key, bool isload);
+        int16_t hashing(std::string _key, bool isload = false);
         void save(std::string _data, std::string _key);
         std::string load(std::string _key);
+        void tablelist(bool _filter_active = true, uint16_t _begin = 0, uint16_t _end = 1000);
     };
-
-    template <typename T>
-    std::vector<T> data;
-
-    template <typename T>
-    void Add(T _data);
 }
-
-#include "hashtable_2.hpp"
 
 #endif
