f666d3aa232a92c06a028155f14b4892f75030a6
team98-k <83960170+team98-k@users.noreply.github.com>
2022-02-09 21:52:47 +0900

    Create hashtable.cpp

---
+++ test/hashtable.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include "hashtable.h"
+#include "Point.h"
+using namespace std;
+
+int main(void)
+{
+    Array<Point<int>> orr1(3);
+    orr1[0]=Point<int>(3, 4);
+    orr1[1]=Point<int>(5, 6);
+    orr1[2]=Point<int>(7, 8);
+
+    for(int i=0; i<orr1.GetArrLen(); i++)
+        orr1[i].ShowPosition();
+    
+    return 0;
+}
